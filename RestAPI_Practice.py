ed71a17048e8ebcf39fba73d3e0d45c11eef06d1
Kourosh <kourosh.emamyy@gmail.com>
2025-03-12 17:03:33 +0330

    video 1

--- RestAPI_Practice.py
+++ RestAPI_Practice.py
@@ -1,1 +1,12 @@
-test
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get('/')
+async def index() -> dict[str : str]:
+    return {'hello': 'world'}
+
+@app.get('/about')
+async def about() -> str:
+    return 'An exceptional Company'
