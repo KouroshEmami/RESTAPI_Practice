c362eefb278141258fcb89fa829b1b213d416f1d
KouroshEmami <kourosh.emamyy@gmail.com>
2025-03-24 06:28:49 -0700

    middle of 7th video - sqlmodel

---
+++ models.py
@@ -0,0 +1,39 @@
+from datetime import date
+from pydantic import BaseModel, field_validator
+from enum import Enum
+from sqlmodel import SQLModel, Field, Relationship
+
+class GenreURLChoices(Enum):
+    ROCK = 'rock'
+    ELECTRONIC = 'electronic'
+    METAL = 'metal'
+    HIP_HOP = 'hip-hop'
+
+class GenreChoices(Enum):
+    ROCK = 'Rock'
+    ELECTRONIC = 'Electronic'
+    METAL = 'Metal'
+    HIP_HOP = 'Hip-Hop'
+
+class AlbumBase(SQLModel):
+    title: str
+    released_date: date
+    band_id: int | None = Field(foreign_key="band.id")
+
+class Album(AlbumBase, table=True):
+    id: int = Field(default=None ,primary_key=True)
+    band: "Band" = Relationship(back_populates="albums")
+
+class BandBase(SQLModel):
+    name : str
+    genre : GenreChoices
+
+class BandCreate(BandBase):
+    albums: list[AlbumBase] | None = None
+    @field_validator('genre', mode="before")
+    def title_case_genre(cls, value):
+        return value.title()
+    
+class Band(BandBase, Table=True):
+    id : int = Field(default=None, primary_key=True)
+    albums: list[Album] = Relationship(back_populates="band")
